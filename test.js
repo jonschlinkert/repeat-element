d6379357b656d94ac0b81a993303d23dd4db62c2
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-26 07:49:46 -0500

    update tests

--- test.js
+++ test.js
@@ -7,11 +7,24 @@
 
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var assert = require('assert');
+var argv = require('minimist')(process.argv.slice(2));
+var files = fs.readdirSync('./benchmark/code');
 var repeat = require('./');
+
+var keys = Object.keys(argv);
+if (keys && keys[1]) {
+  var lib = files.filter(function (fp) {
+    return keys[1] === path.basename(fp, path.extname(fp));
+  });
+  repeat = require(path.resolve('./benchmark/code/' + lib[0]));
+}
 
 it('should repeat the given string:', function () {
   assert.deepEqual(repeat('a', 5), ['a', 'a', 'a', 'a', 'a']);
+  assert.deepEqual(repeat('a', 50), ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a','a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']);
   assert.deepEqual(repeat('a', 1), ['a']);
   assert.deepEqual(repeat('a', 0), []);
 });
